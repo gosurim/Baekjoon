4b237abbaef8ebbc442f0505d482f98a74c1aec6
surim <163095347+gosurim@users.noreply.github.com>
2024-11-22 21:57:15 +0900

    [Silver IV] Title: 문자열 집합, Time: 84 ms, Memory: 36788 KB -BaekjoonHub

---
+++ Python/백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -0,0 +1,12 @@
+import sys
+input=sys.stdin.readline
+n,m=map(int, input().split())
+textList=set([input() for _ in range(n)])
+cnt=0
+
+for _ in range(m):
+    subText=input()
+    if subText in textList:
+        cnt+=1
+
+print(cnt)
