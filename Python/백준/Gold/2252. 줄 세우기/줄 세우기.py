b4c8ac2505a9ce5ecc939b43bed5d4de27917ffd
surim <163095347+gosurim@users.noreply.github.com>
2024-12-13 11:08:43 +0900

    [Gold III] Title: 줄 세우기, Time: 2668 ms, Memory: 39880 KB -BaekjoonHub

--- Python/백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ Python/백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -1,23 +1,25 @@
 from collections import deque
-n,m=map(int, input().split())
-a=[[] for _ in range(n+1)]
-indegree=[0]*(n+1)
 
-for i in range(m):
+
+n,m=map(int, input().split())
+a=[[] for _ in range(n+1)] # 인접 리스트
+indeg=[0]*(n+1) # 진입 차수 리스트
+
+for i in range(m): # 엣지 초기화
     s,e=map(int, input().split())
-    a[s].append(e)
-    indegree[e]+=1
+    a[s].append(e) # 인접 리스트
+    indeg[e]+=1 # 진입 차수 저장
 
 q=deque()
 
 for i in range(1, n+1):
-    if indegree[i]==0:
-        q.append(i)
-
+    if indeg[i]==0: # 진입 0인 것
+        q.append(i) # q에 넣넣
+    
 while q:
     now=q.popleft()
     print(now, end=' ')
     for next in a[now]:
-        indegree[next]-=1
-        if indegree[next]==0:
+        indeg[next]-=1
+        if indeg[next]==0:
             q.append(next)
