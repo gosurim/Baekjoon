ebbcdc6c30954a61fd8f4646e690b175d492b50e
surim <163095347+gosurim@users.noreply.github.com>
2024-12-14 09:46:16 +0900

    [Gold V] Title: 최소비용 구하기, Time: 488 ms, Memory: 62976 KB -BaekjoonHub

--- Python/백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ Python/백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -1,32 +1,32 @@
 from queue import PriorityQueue
 import sys
-
+input=sys.stdin.readline
 
 n=int(input())
 m=int(input())
-arr=[[] for _ in range(n+1)]
+a=[[] for _ in range(n+1)]
 dist=[sys.maxsize]*(n+1)
 vis=[False]*(n+1)
 
 for _ in range(m):
     s,e,w=map(int, input().split())
-    arr[s].append((e,w))
+    a[s].append((e,w))
 
 s,e=map(int, input().split())
 
-def dijkstra(s,e):
-    pq=PriorityQueue()
-    pq.put((0,s))
+def dij(s,e):
+    q=PriorityQueue()
+    q.put((0,s)) # 큐에는 w,node 순
     dist[s]=0
-    while pq.qsize()>0:
-        nowNode=pq.get()
-        now=nowNode[1]
+    while q.qsize() >0:
+        node=q.get()
+        now=node[1]
         if not vis[now]:
             vis[now]=True
-            for x in arr[now]:
-                if not vis[x[0]] and dist[x[0]] > dist[now] + x[1]:
+            for x in a[now]:
+                if not vis[x[0]] and dist[x[0]]>dist[now]+x[1]:
                     dist[x[0]]=dist[now]+x[1]
-                    pq.put((dist[x[0]], x[0]))
+                    q.put((dist[x[0]], x[0]))
     return dist[e]
 
-print(dijkstra(s,e))
+print(dij(s,e))
