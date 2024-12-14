23502350d353553582beaddc97ca01385c156967
surim <163095347+gosurim@users.noreply.github.com>
2024-12-14 13:18:12 +0900

    [Silver I] Title: 경로 찾기, Time: 248 ms, Memory: 33432 KB -BaekjoonHub

---
+++ Python/백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -0,0 +1,16 @@
+n=int(input())
+dis=[[0 for j in range(n)] for i in range(n)]
+
+for i in range(n):
+    dis[i]=list(map(int, input().split()))
+
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            if dis[i][k]==1 and dis[k][j]==1:
+                dis[i][j]=1
+
+for i in range(n):
+    for j in range(n):
+        print(dis[i][j], end=" ")
+    print()
