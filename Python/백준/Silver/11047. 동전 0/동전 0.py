06cd0f9126b541995096ade05789f0d2115eadc6
surim <163095347+gosurim@users.noreply.github.com>
2024-12-14 16:05:16 +0900

    [Silver IV] Title: 동전 0, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,14 @@
+n,k=map(int, input().split())
+arr=[0]*n
+
+for i in range(n):
+    arr[i]=int(input())
+
+cnt=0
+
+for i in range(n-1, -1,-1):
+    if arr[i]<=k:
+        cnt+=int(k/arr[i])
+        k%=arr[i]
+
+print(cnt)
