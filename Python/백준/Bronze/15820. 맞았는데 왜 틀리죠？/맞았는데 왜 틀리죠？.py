7f1a7fab7cc656a12d2e2bb4e3f7516e633a3fa9
surim <163095347+gosurim@users.noreply.github.com>
2025-03-15 15:59:13 +0900

    [Bronze III] Title: 맞았는데 왜 틀리죠?, Time: 36 ms, Memory: 32412 KB, Score: 10 point -BaekjoonHub

---
+++ Python/백준/Bronze/15820. 맞았는데 왜 틀리죠？/맞았는데 왜 틀리죠？.py
@@ -0,0 +1,19 @@
+s, ss=map(int, input().split())
+flag1=True
+flag2=True
+
+for _ in range(s):
+    a,b=map(int ,input().split())
+    if a!=b:
+        flag1=False
+for _ in range(ss):
+    a,b=map(int ,input().split())
+    if a!=b:
+        flag2=False
+
+if flag1 and flag2:
+    print("Accepted")
+elif flag2:
+    print("Wrong Answer")
+else: print("Why Wrong!!!")
+    
